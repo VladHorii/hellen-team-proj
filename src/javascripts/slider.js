6d314a1cdbd03df8e375c2446e944e3fafc62fec
Vlad_Horii <vladhorii@yandex.ru>
2021-04-07 17:00:04 +0300

    Завершение работы

--- src/javascripts/slider.js
+++ src/javascripts/slider.js
@@ -9,6 +9,7 @@ import img7 from '../images/reviews/Ellipse-15.png';
 
 const img = [img1, img2, img3, img4, img5, img6, img7];
 let curSlide = -1;
+let isMobile = false;
 
 $(document).ready(function () {
   $('.js-slider').slick({
@@ -27,71 +28,60 @@ const createImg = source => `<img
       height="54" />`;
 //
 //
-
 const renderMobileDots = () => {
+  console.log('+');
   const dotsRefs = document.querySelectorAll('.slick-dots li');
-  const currentDotRef = document.querySelector('.slick-dots li.slick-active button');
+  const numOfCurrentDot = Number($('.slick-active').attr('data-slick-index')) + 1;
 
-  // console.log(currentDotRef.data('role'));
-  // const numOfCurrentDot = currentDotRef.getAttribute('aria-controls');
-  const jqueryElem = $('.slick-dots li.slick-active button');
-  console.log('renderMobileDots ~ jqueryElem', jqueryElem);
-  const numOfCurrentDot = jqueryElem.data('role');
-  console.log('renderMobileDots ~ numOfCurrentDot', numOfCurrentDot);
-  // const numOfCurrentDot = Number(currentDotRef.textContent) - 1;
-  // console.log('numOfCurrentDot', numOfCurrentDot);
-  return;
   if (curSlide === numOfCurrentDot) {
     return console.log('curSlide === numOfCurrentDot');
   }
 
   dotsRefs.forEach((el, index) => {
-    // console.log(index);
-    // console.log(index === numOfCurrentDot);
-    // console.log(index === numOfCurrentDot - 1);
-    // console.log(index === numOfCurrentDot + 1);
-    // console.log('=====');
-
     if (
       index === numOfCurrentDot ||
       index === numOfCurrentDot - 1 ||
-      index === numOfCurrentDot + 1
+      index === numOfCurrentDot + 1 ||
+      (numOfCurrentDot === img.length && index === img.length - 3) ||
+      (numOfCurrentDot === img.length && index === img.length - 2) ||
+      (numOfCurrentDot === img.length - 1 && index === img.length - 3) ||
+      (numOfCurrentDot === 0 && index === 2)
     ) {
-      console.log('good', index);
       const elWithImg = createImg(img[index]);
       el.firstChild.innerHTML = elWithImg;
       el.classList.remove('visually-hidden');
     } else {
-      console.log('bad', index);
       el.classList.add('visually-hidden');
-    }
-    if (numOfCurrentDot === 0 && index === 2) {
-      const elWithImg = createImg(img[index]);
-      el.firstChild.innerHTML = elWithImg;
-      el.classList.remove('visually-hidden');
-    }
-    if (numOfCurrentDot === img.length - 1 && index === img.length - 3) {
-      const elWithImg = createImg(img[index]);
-      el.firstChild.innerHTML = elWithImg;
-      el.classList.remove('visually-hidden');
     }
   });
   curSlide = numOfCurrentDot;
-  // console.log('render mobile dots end');
 };
-$('.js-slider').on('afterChange', function () {
-  renderMobileDots();
-  console.log('afterChange');
-});
-$('.js-slider').on('init', function (event, slick) {
-  const dotsRefs = document.querySelectorAll('.slick-dots li');
 
+$('.js-slider').on('afterChange', function () {
+  if (isMobile) {
+    renderMobileDots();
+  }
+});
+function initNav() {
   if (window.matchMedia('(min-width: 768px)').matches) {
+    const dotsRefs = document.querySelectorAll('.slick-dots li');
     dotsRefs.forEach((el, index) => {
+      el.classList.remove('visually-hidden');
       const elWithImg = createImg(img[index]);
       el.firstChild.innerHTML = elWithImg;
     });
   } else {
+    isMobile = true;
     renderMobileDots();
   }
+}
+
+window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  // if (!e.matches) return;
+  console.log('change');
+  initNav();
+});
+
+$('.js-slider').on('init', function (event, slick) {
+  initNav();
 });
