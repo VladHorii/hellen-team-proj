f093f282696fd768ea5a590178d3f41bd30e1baa
Vlad_Horii <vladhorii@yandex.ru>
2021-04-08 12:09:21 +0300

    Настройка плавного скролла

---
+++ src/js/scrollTo.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+var $page = $('html, body');
+$('a[href*="#"]').click(function () {
+  $page.animate(
+    {
+      scrollTop: $($.attr(this, 'href')).offset().top,
+    },
+    400,
+  );
+  return false;
+});
